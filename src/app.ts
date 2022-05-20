898a06b864ed876b22a8e60e1579663627645b0e
dohyun.lee <lhl890@naver.com>
2022-05-21 01:13:39 +0900

    앱 시작 시 인증 미들웨어를 사용하지 않게 수정

--- src/app.ts
+++ src/app.ts
@@ -15,5 +15,13 @@ export const app = express();
 export const port = process.env.PORT;
 
 app.use(express.json());
-app.use('/events', auth, convertUUIDToUseIndex, events);
-app.use('/point', auth, point);
+
+/**
+ * 테스트하기 번거로울 것 같아 일단 주석처리하였습니다.
+ * 만약 서버 시작 시 생성되는 토큰을 테스트하려면 아래의 코드를 주석 해제하고 
+ * 기존 코드(auth 미들웨어가 적용되지 않은)는 주석 처리하면 됩니다.
+ */
+// app.use('/events', auth, convertUUIDToUseIndex, events);
+// app.use('/point', auth, point);
+app.use('/events', convertUUIDToUseIndex, events);
+app.use('/point', point);
