9775aeb1348068a0f1f43578b0b64f9558a8731b
dohyun.lee <lhl890@naver.com>
2022-05-20 19:59:59 +0900

    서버 시작 시 DB 초기화 코드 수정

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
-import { createTables, dropTables, initData } from './init/init';
+import {
+  createIndexes,
+  createTables,
+  dropIndexes,
+  dropTables,
+  initData,
+} from './init/init';
 import { connectDB } from './models/db';
 import jwt from './utils/jwt';
 import { app, port } from './app';
 
 async function main() {
   connectDB();
-  // DB 테이블 전체 삭제를 원할 시 아래 코드 주석 해제
+
+  // db 초기화
+  await dropIndexes();
   await dropTables();
-  // DB 테이블 전체 생성을 원하면 아래 코드 주석 해제
   await createTables();
-  // 데이터 초기화 시 주석 해제
+  await createIndexes();
   await initData();
 
   const accessToken = jwt.sign({
