32f098744d0e242b2e4fa33cf7e9d4fab82430c8
Dohyun Lee <lhl890@naver.com>
2023-08-17 22:41:26 +0900

    [fix] createContext 미들웨어 오류 수정

--- src/domain/user/dto/CreateUserDto.ts
+++ src/domain/user/dto/CreateUserDto.ts
@@ -1,19 +1,11 @@
-import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsStrongPassword, MinLength, minLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   email!: string;
 
-  @IsStrongPassword(
-    {
-      minLength: 8,
-      minSymbols: 2,
-    },
-    {
-      message: '비밀번호는 최소 8자리 이상 2개의 특수문자가 포함돼야합니다.',
-    },
-  )
+  @MinLength(8)
   @IsNotEmpty()
   password!: string;
 }
--- src/middleware/createContext.ts
+++ src/middleware/createContext.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { Context } from '../context';
+import { initTypeOrmDataSource, setTypeOrmDataSource } from '../db';
 
-export function createContext() {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const context = new Context();
-    req.context = context;
-    next();
-  };
+export async function createContext(req: Request, res: Response, next: NextFunction) {
+  const context = new Context();
+  req.context = context;
+  const dataSource = await initTypeOrmDataSource();
+  setTypeOrmDataSource(context, dataSource);
+  next();
 }
